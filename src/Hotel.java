90b21dee28ddd52ea0ed8bc5d600df013f6fc75b
Arttero <artt3ro@gmail.com>
2025-05-06 12:09:15 +0200

    Tak

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Hotel {
     ArrayList<Room> rooms = new ArrayList<>();
@@ -18,24 +19,57 @@ public class Hotel {
     public void removeRoom(String roomNumber) throws Exception {
         try {
             for (Room room : rooms) {
-                if (room.getRoomNumber() == roomNumber) {
+                if (room.getRoomNumber().equals(roomNumber)) {
                     rooms.remove(room);
+                    break;
                 }
             }
         } catch (Exception e) {
-            System.out.println("Niepoprawne room nie istnieje!");
+            System.out.println("Błąd: " + e.getMessage());
         }
     }
+
+    //dodatkowe
+    public void printRooms() {
+        if (rooms.isEmpty()) {
+            System.out.println("Brak pokoi w hotelu.");
+        } else {
+            for (Room room : rooms) {
+                System.out.println(room);
+            }
+        }
+    }
+
 
     public void addRoom(String roomNumber, int beds) {
         this.rooms.add(new Room(roomNumber, beds));
     }
 
     public void saveRoomsToFile(String fileName) throws IOException {
-        // nie pamietam
+        try {
+            FileWriter fileWriter = new FileWriter(fileName);
+            for (Room room : rooms) {
+                fileWriter.write(room.toString());
+                fileWriter.write("\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        }
     }
 
-    public void loadRoomsFromFile(String fileName) throws IOException {
-        // nie pamietam
-    }
+//    public void loadRoomsFromFile(String fileName) throws IOException {
+//        try {
+//            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+//            while (bufferedReader.readLine() != null) {
+//                String roomNumber = bufferedReader.readLine();
+//                int beds = Integer.parseInt(bufferedReader.readLine());
+//                int pricePerNight = Integer.parseInt(bufferedReader.readLine());
+//                addRoom(roomNumber, beds);
+//            }
+//            bufferedReader.close();
+//        } catch (IOException e) {
+//            System.out.println("Błąd: " + e.getMessage());
+//        }
+//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
         //w main
         //tablica kilku pokoi (obieekty room)
         //wypisz pokoje z cena ponizej 200zl(petla i instrukcja warunkowa)
@@ -13,30 +9,37 @@ public class Main{
         System.out.println("\nCzęść na 3\n");
 
         Room[] rooms = {
-                new Room("Jeden",2),
-                new Room("Dwa",3, 200),
-                new Room("3",4, 250),
-                new Room("4",4, 300),
+                new Room("1",1,100),
+                new Room("2",2,200),
+                new Room("1a",2),
+                new Room("1c",3, 200),
+                new Room("3a",4, 250),
+                new Room("4b",4, 300),
                 new Room("5",4, 300),
                 new Room("6",4, 300),
         };
+        System.out.println("Pokoje z ceną poniżej 200zł:");
         for (Room room : rooms) {
             if (room.getPricePerNight() < 200) {
                 System.out.println(room);
             }
         }
+        System.out.println();
+
+        System.out.println("Po zamianie malych liter na duze:");
         for (Room room : rooms) {
-            System.out.println(room.getRoomNumber().toUpperCase());
+            System.out.println("Room: " +
+                    "roomNumber= " + room.getRoomNumber().toUpperCase() +
+                    ", beds= " + room.getBeds() +
+                    ", pricePerNight= " + room.getPricePerNight() + " zł/noc");
         }
+        System.out.println();
 
         System.out.println("\nCzęść na 4\n");
-
         Hotel hotel = new Hotel();
 
-        hotel.loadRoomsFromFile("rooms.txt");
-
-        hotel.addRoom(new Room("2",2,60));
-        hotel.addRoom(new Room("3",2,60));
+        hotel.addRoom(new Room("2a",2,60));
+        hotel.addRoom(new Room("3b",2,60));
         hotel.addRoom(new Room("4",3,100));
         hotel.addRoom(new Room("5",3,100));
         hotel.addRoom(new Room("6",4,120));
@@ -44,13 +47,13 @@ public class Main{
 
         System.out.println("Pokoje z 3 lozkami:");
         hotel.findByBeds(3);
-
-        System.out.println("Po usunieciu pokoi:");
-
         System.out.println();
-        hotel.removeRoom("6");
-        hotel.removeRoom("3");
 
+        hotel.removeRoom("6");
+        hotel.removeRoom("3b");
+        System.out.println("Po usunieciu pokoi:");
+        hotel.printRooms();
+        System.out.println();
 
         hotel.saveRoomsToFile("rooms.txt");
     }
